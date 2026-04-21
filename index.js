db9420be54696f73c23b2dd16fcb66a38c50b376
zoomerxbt <lucidzyre@gmail.com>
2026-04-21 22:20:17 +0200

    Refactored gameboard module

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
-function Gameboard() {
+function gameboard() {
   let rows = 3;
   let columns = 3;
 
   // create two-dimensional array
-  const board = Array.from({ length: rows }, () => new Array(columns).fill(0));
+  const board = Array.from({ length: rows }, () => new Array(columns).fill(""));
   const getBoard = () => board;
 
   //  We will need a method to place mark on any available cell that has 0 placeholder
   const placeMark = (player, row, column) => {
     // Validate if a index is open
     if (board[row][column] !== "") {
-      return false;
+      return "Invalid move";
     }
     board[row][column] = player;
-    return true;
+    return board;
   };
-  return { getBoard, placeMark };
+
+  const printBoard = () => {
+    return board;
+  };
+
+  return { getBoard, placeMark, printBoard };
 }
+
+let newGame = gameboard();
+
+console.table(newGame.placeMark("X", 1, 2));
+console.table(newGame.placeMark("0", 2, 2));
+console.table(newGame.placeMark("0", 1, 1));
+console.table(newGame.placeMark("0", 2, 1));
