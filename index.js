e848077237dcb82137330c0e456c20395482125b
zoomerxbt <lucidzyre@gmail.com>
2026-04-21 19:48:26 +0200

    Updating placeMark module

--- index.js
+++ index.js
@@ -2,19 +2,18 @@ function Gameboard() {
   let rows = 3;
   let columns = 3;
 
-  // creating two-dimensional array
+  // create two-dimensional array
   const board = Array.from({ length: rows }, () => new Array(columns).fill(0));
   const getBoard = () => board;
 
   //  We will need a method to place mark on any available cell that has 0 placeholder
-
-  return { getBoard };
+  const placeMark = (player, row, column) => {
+    // Validate if a index is open
+    if (board[row][column] !== "") {
+      return false;
+    }
+    board[row][column] = player;
+    return true;
+  };
+  return { getBoard, placeMark };
 }
-
-let rows = 3;
-let columns = 3;
-
-// creating two-dimensional array
-const board = Array.from({ length: rows }, () => new Array(columns).fill(0));
-
-console.table(board);
