28075f3213b963a71d47c0b8741eb2444acdba87
zoomerxbt <lucidzyre@gmail.com>
2026-04-21 19:00:10 +0200

    Adding a new sub-module for placing a mark on a empty block

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-function GameBoard() {
-  let board = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""],
-  ];
+function Gameboard() {
+  let rows = 3;
+  let columns = 3;
+
+  // creating two-dimensional array
+  const board = Array.from({ length: rows }, () => new Array(columns).fill(0));
+  const getBoard = () => board;
+
+  //  We will need a method to place mark on any available cell that has 0 placeholder
+
+  return { getBoard };
 }
+
+let rows = 3;
+let columns = 3;
+
+// creating two-dimensional array
+const board = Array.from({ length: rows }, () => new Array(columns).fill(0));
+
+console.table(board);
