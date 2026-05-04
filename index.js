820b91d9807c0d0ea5cff79129af6e6a2950773f
Zoomerxbt <85997099+Zoomerxbt@users.noreply.github.com>
2026-05-04 14:17:29 +0200

    Updated the placemark parameter from player to token

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ function gameboard() {
   const board = Array.from({ length: rows }, () => new Array(columns).fill(""));
   const getBoard = () => board;
 
-  //  We will need a method to place mark on any available cell that has 0 placeholder
-  const placeMark = (player, row, column) => {
+  //  We will need a method that will insert a token in an empty cell
+  const placeMark = (token, row, column) => {
     // Validate if a index is open
     if (board[row][column] !== "") {
       return "Invalid move";
     }
-    board[row][column] = player;
+
+    board[row][column] = token;
     return board;
   };
 
@@ -23,9 +24,40 @@ function gameboard() {
   return { getBoard, placeMark, printBoard };
 }
 
-let newGame = gameboard();
+// The game controller will be responsible for the flow of the game
 
-console.table(newGame.placeMark("X", 1, 2));
-console.table(newGame.placeMark("0", 2, 2));
-console.table(newGame.placeMark("0", 1, 1));
-console.table(newGame.placeMark("0", 2, 1));
+function gameController(player1 = "Player 1", player2 = "Player 2") {
+  const board = gameboard();
+
+  const players = [
+    {
+      player1: player1,
+      token: 1,
+    },
+    {
+      player2: player2,
+      token: 2,
+    },
+  ];
+
+  let activePlayer = player[0];
+
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  };
+  const getActivePlayer = () => activePlayer;
+
+  const printNewRound = () => {
+    board.printBoard();
+    console.log(`${getActivePlayer().name}'s turn.`);
+  };
+
+  switchPlayerTurn();
+
+  return {};
+}
+
+let game = gameboard();
+
+console.table(game.placeMark("X", 0, 0));
+console.table(game.placeMark("O", 0, 0));
